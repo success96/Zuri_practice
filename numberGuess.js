cd888ebbedda8778fb9e7b97c0d1896f0857f884
Success96 <lfawas6@gmail.com>
2022-11-06 17:56:48 +0100

    Added numberguss.js

---
+++ numberGuess.js
@@ -0,0 +1,29 @@
+//This a number guessing javascript application
+//It prompts the user to guess a number within a given range , if guessed correctly, the user is moved to the next stage where the range is further increase by 1.
+const prompt = require("prompt-sync")();
+console.log("WELCOME TO THE NUMBER GUESSING GAME!")
+let stage = 1;
+let points = 0;
+const guess = function(range) {
+  const correct = Math.floor(Math.random() * range)
+  const number = Number(prompt(`Guess a number between 0 and ${range} && Enter '00' to Exit  `));
+  if (number === 00){
+    console.log('Thank you for playing!');
+    return
+    
+  }
+  else if (number === correct) {
+    points++
+    console.log("Woow, That's correct")
+    console.log(`You have ${points} points`)
+    stage++
+    console.log(`You moved to Stage ${stage}`);
+    range++;
+    guess(range);
+  } else {
+    console.log("Wrong! Try again!");
+    guess(range)
+  }
+}
+
+guess(3)
